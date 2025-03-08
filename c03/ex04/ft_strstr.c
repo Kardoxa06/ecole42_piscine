f584873ddf88b92b75453e22c2a41c1706daa331
Ozer Abdullah Kose <ozkose@k1m22s01.42kocaeli.com.tr>
2025-03-08 16:33:13 +0300

    C02 - C03 Güncellemeleri

--- c03/ex04/ft_strstr.c
+++ c03/ex04/ft_strstr.c
@@ -6,27 +6,38 @@
 /*   By: ozkose <ozkose@student.42kocaeli.com.tr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 12:19:59 by ozkose            #+#    #+#             */
-/*   Updated: 2025/03/05 12:27:56 by ozkose           ###   ########.tr       */
+/*   Updated: 2025/03/08 16:32:03 by ozkose           ###   ########.tr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strstr(char *str, char *to_find)
 {
 	int	i;
-	int p;
+	int	p;
 
-	p = 0;
 	i = 0;
-
 	if (to_find[i] == '\0')
 		return (str);
 	while (str[i])
 	{
-		while (str[i] && to_find[i] && str[i] == to_find[i])
+		p = 0;
+		while (str[i] && str[i + p] == to_find[p])
 		{
-			i++;
+			if (to_find[p + 1] == '\0')
+			{
+				return (&str[i]);
+			}
 			p++;
 		}
-
+		i++;
 	}
+	return (0);
 }
+
+// #include <stdio.h>
+// int main(void)
+// {
+// 	char str[] = "ozer bir marabadır";
+// 	char to_find[] = "ozer";
+// 	printf("%s", ft_strstr(str, to_find));
+// }
